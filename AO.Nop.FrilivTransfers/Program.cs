fae8cabe80ecd00a3ae5f41b2a4c3042249f1800
Axel Dalen Andersen <axelandersen@gmail.com>
2018-09-26 06:21:05 +0200

    Initial commit

---
+++ AO.Nop.FrilivTransfers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AO.Nop.FrilivTransfers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
